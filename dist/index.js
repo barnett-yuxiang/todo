376e9f235714e6fbbb876554454b98296b9a7834
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-09-11 23:57:25 +0800

    Refactor code to use JsonTodoCollection for storing todos

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,17 @@
 import { TodoItem } from "./todoItem.js";
-import { TodoCollection } from "./todoCollection.js";
 import inquirer from "inquirer";
+import { JsonTodoCollection } from "./jsonTodoCollection.js";
 let todos = [
-    new TodoItem(1, "Buy Flowers"),
-    new TodoItem(2, "Get Shoes"),
-    new TodoItem(3, "Collect Tickets"),
-    new TodoItem(4, "Call Joe", true),
+    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)
 ];
-let collection = new TodoCollection("Adam", todos);
+let collection = new JsonTodoCollection("Adam", todos);
 let showCompleted = true;
 function displayTodoList() {
-    console.clear();
-    console.log(`${collection.userName}'s Todo List ` + `${collection.getItemCounts().incomplete} items to do`);
-    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+    console.log(`${collection.userName}'s Todo List `
+        + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(showCompleted)
+        .forEach(item => item.printDetails());
 }
 var Commands;
 (function (Commands) {
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 import { TodoItem } from "./todoItem.js";
 import { TodoCollection } from "./todoCollection.js";
 import inquirer from "inquirer";
+import { JsonTodoCollection } from "./jsonTodoCollection.js";
 
 let todos: TodoItem[] = [
-  new TodoItem(1, "Buy Flowers"),
-  new TodoItem(2, "Get Shoes"),
-  new TodoItem(3, "Collect Tickets"),
-  new TodoItem(4, "Call Joe", true),
-];
+  new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+  new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 
-let collection: TodoCollection = new TodoCollection("Adam", todos);
+let collection: TodoCollection = new JsonTodoCollection("Adam", todos);
 let showCompleted = true;
 
 function displayTodoList(): void {
-  console.clear();
-  console.log(`${collection.userName}'s Todo List ` + `${collection.getItemCounts().incomplete} items to do`);
-  collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+  console.log(`${collection.userName}'s Todo List `
+    + `(${collection.getItemCounts().incomplete} items to do)`);
+  collection.getTodoItems(showCompleted)
+    .forEach(item => item.printDetails());
 }
 
 enum Commands {
